535cee329912866c5ab2f6caca5f857156ab0134
stefc <stefan.boether@gmail.com>
2015-06-18 19:16:29 +0200

    isc_attach_database now works & getDialect also but still don't get fb_interpret to work :(

--- FirebirdTest/FbConnection.swift
+++ FirebirdTest/FbConnection.swift
@@ -10,11 +10,16 @@ import Foundation
 
 func msgToString(status : UnsafeMutablePointer<UnsafePointer<CLong>>) -> String? {
 	
-	var buffer = [CChar](count: 1024, repeatedValue: 0)
+	var buffer = [CChar](count: 100, repeatedValue: 0)
 	
-	let r = fb_interpret(&buffer, 20, status)
+	// let r = isc_interprete(&buffer, status)
+	let r = fb_interpret(&buffer, CUnsignedInt(40), status)
 	if r != 0 {
 		return String.fromCString(buffer)
+	}
+	else
+	{
+		println(" fb_interpret returns : \(r)")
 	}
 	return nil
 }
@@ -47,42 +52,37 @@ class FirebirdConnection {
 	
 	init(database: String, username: String, password: String) {
 		
-		
+		// determine the dialect from the database
 		func getDbDialect() -> Int {
 			
 			var dbp  = [CChar](count: 2, repeatedValue: 0)
-			dbp[0] = CChar(62/*isc_info_db_sql_dialect*/)
+			dbp[0] = CChar(isc_info_db_SQL_dialect)
 			dbp[1] = CChar(isc_info_end)
 			
-			var data = [Int8](count: 39, repeatedValue: 0)
+			var data = [CChar](count: 39, repeatedValue: 0)
 			
 			var result = -1
 			
-			/*
-			isc_database_info(<#UnsafeMutablePointer<ISC_STATUS>#>, <#UnsafeMutablePointer<isc_db_handle>#>, <#Int16#>, <#UnsafePointer<ISC_SCHAR>#>, <#Int16#>, <#UnsafeMutablePointer<ISC_SCHAR>#>)
-			*/
-			let ret = isc_database_info(&fStatus, &fHandle, CShort(count(dbp)), &dbp,
-				CShort(count(data)), &data)
+			let ret = isc_database_info(&fStatus, &fHandle, CShort(count(dbp)), &dbp, CShort(count(data)), &data)
 			if ret != 0 {
-				println("GetDBDialect")
 				return -1
 			}
 			
-			/*x := 0;
-			while x < 40 do
-			case ResBuf[x] of
-			isc_info_db_sql_dialect :
-			begin
-			//Inc(x);
-			Len := isc_portable_integer(@ResBuf[x+1], 2);
-			Result := isc_portable_integer(@ResBuf[x+3], Len);
-			Inc(x, Len+3);
-			end;
-			isc_info_end : Break;
-			else inc(x);
-			end;
-			end;
-			*/
+			var p : UnsafePointer<CUnsignedChar> = UnsafePointer<CUnsignedChar>(data)
+			while (p != nil) && (p[0] != CUnsignedChar(isc_info_end)) {
+				if p[0] == CUnsignedChar(isc_info_db_SQL_dialect) {
+					p = p.successor()
+					let len = Int(isc_portable_integer(p, CShort(2)))
+					p = p.successor().successor()
+					result = Int(isc_portable_integer(p, CShort(len)))
+					for _ in 1...len { p = p.successor() }
+				}
+				else
+				{
+					p = p.successor()
+				}
+			}
+			
 			return result
 		}
 		
@@ -104,11 +104,13 @@ class FirebirdConnection {
 		let ret = isc_attach_database(&fStatus, CShort(count(db)), &db, &fHandle, CShort(count(dbp)), &dbp)
 		if ret != 0 {
 			if (fStatus[0] == 1) && (fStatus[1] != 0) {
-				println(msgToString(UnsafeMutablePointer<UnsafePointer<CLong>>(fStatus)))
+				var p = UnsafeMutablePointer<UnsafePointer<CLong>>(fStatus)
+				println(msgToString(p))
 			}
 		}
 		
 		fDialect = getDbDialect()
+		println( "Dialect : \(fDialect)")
 		
 	}
 	
